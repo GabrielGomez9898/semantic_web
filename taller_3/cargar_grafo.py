52d9788639f9ac4c93f633f2eea675e41007f7db
GabrielGomez9898 <gomezga@javeriana.edu.co>
2026-09-26 13:07:11 -0500

    cargar grafo y validación de sintaxis

---
+++ taller_3/cargar_grafo.py
@@ -0,0 +1,15 @@
+from rdflib import Graph
+
+archivo = "universidad-base.ttl"
+
+g = Graph()
+
+try:
+    g.parse(archivo, format="turtle")
+
+    print("✅ El archivo Turtle es válido.")
+    print(f"Triples cargados: {len(g)}")
+
+except Exception as e:
+    print("❌ Error al cargar el archivo:")
+    print(e)
